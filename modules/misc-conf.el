72f9da963d7fee632d2b80c187da2f66c9afba0f
Zhengyang Liu <zhengyangl@hotmail.com>
2014-11-21 22:11:59 +0800

    [BUGFIX] fix scrollbar error

--- modules/misc-conf.el
+++ modules/misc-conf.el
@@ -31,9 +31,9 @@
 (setq initial-scratch-message nil)
 
 ;;disable scroll bar
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
-(menu-bar-mode -1)
+;;(scroll-bar-mode -1)
+;;(tool-bar-mode -1)
+;;(menu-bar-mode -1)
 
 
 ;;disable emacs startup message
