2889940489ba3448eebf85fed276cc76fb63afe0
Zhengyang Liu <zhengyangl@hotmail.com>
2015-09-07 16:00:56 +0800

    google c-style

--- modules/c-mode-conf.el
+++
@@ -1,7 +0,0 @@
-(add-hook 'c-mode-common-hook
-          (lambda ()
-            (show-paren-mode 1)
-            (setq indent-tabs-mode t)
-            (c-set-style "stroustrup")
-            (setq tab-width 3)))
-(provide 'c-mode-conf)
--- modules/cpp-mode-conf.el
+++ modules/cpp-mode-conf.el
@@ -1,17 +1,9 @@
+
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
 ; style I want to use in c++ mode
-(c-add-style "cern-style"
-             '("stroustrup"
-               (indent-tabs-mode . nil)        ; use spaces rather than tabs
-               (c-basic-offset . 3)            ; indent by four spaces
-               (c-offsets-alist . ((inline-open . 0)  ; custom indentation rules
-                                   (brace-list-open . 0)
-                                   (statement-case-open . +)))))
 
-(defun my-c++-mode-hook ()
-  (c-set-style "cern-style"))        ; use my-style defined above
-
-(add-hook 'c++-mode-hook 'my-c++-mode-hook)
+(add-hook 'c-mode-common-hook 'google-set-c-style)
+(add-hook 'c-mode-common-hook 'google-make-newline-indent)
 
 (require 'cc-mode)
 (require 'semantic)
