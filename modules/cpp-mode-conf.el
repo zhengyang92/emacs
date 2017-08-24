e898ce2987485f88b5fa2b24d4a54dffbc19a34f
Zhengyang Liu <liuz@cs.utah.edu>
2017-08-24 08:00:19 -0600

    Desable function

--- modules/cpp-mode-conf.el
+++ modules/cpp-mode-conf.el
@@ -13,11 +13,11 @@
 
 (semantic-mode 1)
 
-(require 'function-args)
-(fa-config-default)
+;;(require 'function-args)
+;;(fa-config-default)
 (define-key c-mode-map  [(control tab)] 'moo-complete)
 (define-key c++-mode-map  [(control tab)] 'moo-complete)
-(define-key c-mode-map (kbd "M-o")  'fa-show)
-(define-key c++-mode-map (kbd "M-o")  'fa-show)
+;;(define-key c-mode-map (kbd "M-o")  'fa-show)
+;;(define-key c++-mode-map (kbd "M-o")  'fa-show)
 
 (provide 'cpp-mode-conf)
